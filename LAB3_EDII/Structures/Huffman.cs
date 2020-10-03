de50886f9ea00398d073ecb2b141e054c443ff18
Bushinii <jorgeg.baldizon@gmail.com>
2020-10-03 01:35:33 -0600

    Huffman Tree Insert Started

--- LAB3_EDII/Structures/Huffman.cs
+++ LAB3_EDII/Structures/Huffman.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.MemoryMappedFiles;
 using System.Text;
 
 namespace CustomGenerics.Structures
 {
-    class Huffman : ICompressor
+    class Huffman<T> : ICompressor
     {
+        #region Variables
+        public HuffmanNode<T> Root;
+        public int NextId = 1;
+        #endregion
+
         public void Compress()
         {
             throw new NotImplementedException();
@@ -15,5 +21,15 @@ namespace CustomGenerics.Structures
         {
             throw new NotImplementedException();
         }
+        public PQNode<T> Insert(PQNode<T> Node1, PQNode<T> Node2)
+        {
+            HuffmanNode<T> Right = new HuffmanNode<T>(Node1.Key, Node1.Priority);
+            HuffmanNode<T> Left = new HuffmanNode<T>(Node2.Key, Node2.Priority);
+            byte Id = Convert.ToByte("N" + Convert.ToString(NextId));
+            HuffmanNode<T> Father = new HuffmanNode<T>(Id, (Node1.Priority + Node2.Priority));
+            Father.Rightson = Right;
+            Father.Leftson = Left;
+        }
+
     }
 }
---
+++ LAB3_EDII/Structures/HuffmanNode.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomGenerics.Structures
+{
+    class HuffmanNode<T>
+    {
+        #region Variables
+        public byte ID;
+        public double Chance;
+        public HuffmanNode<T> Rightson;
+        public HuffmanNode<T> Leftson;
+        #endregion
+        public HuffmanNode(byte newid, double newchance)
+        {
+            ID = newid;
+            Chance = newchance;
+        }
+    }
+}
