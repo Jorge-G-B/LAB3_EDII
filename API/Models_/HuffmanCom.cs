c34696f10585fb8db65605f3b9e48530f73dfa3e
Mansi1108 <mansillamelissa@gmail.com>
2020-10-06 22:21:16 -0600

    Basic API

---
+++ API/Models_/HuffmanCom.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models_
+{
+    public class HuffmanCom 
+    {
+        public string originalName { get; set; }
+        public string compressedFilePath { get; set; }
+        public double compressionRatio { get; set; }
+        public double compressionFactor { get; set; }
+        public double reductionPercentage { get; set; }
+
+    }
+}
