04b7fd3e10ecc7e03561bb7d6d170924fbd3935a
Mansi1108 <mansillamelissa@gmail.com>
2020-10-09 13:58:57 -0600

    Methods Fixed

--- API/Models_/HuffmanCom.cs
+++ API/Models_/HuffmanCom.cs
@@ -25,12 +25,14 @@ namespace API.Models_
         public void SetAttributes(string path, string prevName, string newName)
         {
             var CountBytesO = System.IO.File.ReadAllBytes($"{path}/{prevName}");
-            int BNO = CountBytesO.Count();
+            var FileP = $"{path}/{newName}";
+            double BNO = CountBytesO.Count();
             var CountBytesC = System.IO.File.ReadAllBytes($"{path}/{newName}");
-            int BNC = CountBytesC.Count();
+            double BNC = CountBytesC.Count();
 
             OriginalName = prevName;
             CompressedName = newName;
+            CompressedFilePath = FileP;
             GetRatio(BNC, BNO);
             GetFactor(BNC, BNO);
             RPercentage();
@@ -40,7 +42,7 @@ namespace API.Models_
             var file = new FileStream($"{path}/CompressionHist", FileMode.OpenOrCreate);
             var BytesToWrite = JsonSerializer.Serialize(Storage.Instance.HistoryList, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
             using var writer = new StreamWriter(file);
-            writer.Write(BytesToWrite);
+            writer.WriteLine(BytesToWrite);
             writer.Close();
             file.Close();
         }
@@ -55,25 +57,25 @@ namespace API.Models_
                 var list = JsonSerializer.Deserialize<List<HuffmanCom>>(content, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
                 foreach (var item in list)
                 {
-                    Storage.Instance.HistoryList.Add(item);
+                    Storage.Instance.HistoryList.Add(item);                  
                 }
             }
             file.Close();
         }
 
-        public void GetRatio(int bNC, int bNO)
+        public void GetRatio(double bNC, double bNO)
         {
-            CompressionRatio = bNC / bNO;
+            CompressionRatio = Math.Round(bNC / bNO,4);
         }
 
-        public void GetFactor(int NumBC, int NumBO)
+        public void GetFactor(double NumBC, double NumBO)
         {
-            CompressionFactor = NumBO / NumBC;
+            CompressionFactor = Math.Round(NumBO / NumBC,3);
         }
 
         public void RPercentage()
         {
-            ReductionPercentage = CompressionRatio * 100;
+            ReductionPercentage = Math.Round(CompressionRatio * 100,5);
         }
     }
 }
